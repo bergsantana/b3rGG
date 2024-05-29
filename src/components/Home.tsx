8e34d6413eb955ba6e63040c658fcaba6e7e7b55
berg <wdmsantana@gmail.com>
2024-05-29 12:55:58 -0400

    resume work

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,41 +1,46 @@
  
-import './Home.css'
-import { Outlet, useNavigate } from "react-router-dom";
-import Sidebar from "./Sidebar/Sidebar";
-import { useState } from 'react';
-import { SelectedRoute } from '../utils/SelectedRoute';
-import { classes } from '../utils/classes';
+// import './Home.css'
+// import { Outlet, useNavigate } from "react-router-dom";
+// import Sidebar from "./Sidebar/Sidebar";
+// import { useState } from 'react';
+// import { SelectedRoute } from '../utils/SelectedRoute';
+// import { classes } from '../utils/classes';
 
 
-export default function Home(){
+// export default function Home(){
     
-    const [currentRoute, updatedCurrentRoute] = useState(SelectedRoute.HOME)
+//     const [currentRoute, updatedCurrentRoute] = useState(SelectedRoute.HOME)
 
-    const theme = classes
+//     const theme = classes
 
-    const navigate = useNavigate()
+//     const navigate = useNavigate()
 
-    const goToHome = () => navigate('/')
+//     const goToHome = () => navigate('/')
 
-    const goToCreateMenu = () => navigate('/create-menu')
+//     const goToCreateMenu = () => navigate('/create-menu')
 
-    const goToDietCreator = () => navigate('/diet')
+//     const goToDietCreator = () => navigate('/diet')
 
-    return (
-    <>
-        <div className={`mainContainer ${theme[currentRoute]}`}>
-            <div className="sidebarContainer">
-                 <Sidebar  routes={[goToHome , goToCreateMenu, goToDietCreator]} currentRoute={currentRoute} updateRouteTheme={updatedCurrentRoute} />
-            </div>
-            <div className="viewContainer">
-                <Outlet />
-            </div>
-            <div className="footerContainer">
-                <button>footer btn</button>
-            </div>
-        </div>
-        </>
-    )
+
+
+
+ 
+
+//     return (
+//     <>
+//         <div className={`mainContainer ${theme[currentRoute]}`}>
+//             <div className="sidebarContainer">
+//                  <Sidebar  routes={[goToHome , goToCreateMenu, goToDietCreator]} currentRoute={currentRoute} updateRouteTheme={updatedCurrentRoute} />
+//             </div>
+//             <div className="viewContainer">
+//                 <Outlet />
+//             </div>
+//             <div className="footerContainer">
+//                 <button  >footer btn</button>
+//             </div>
+//         </div>
+//         </>
+//     )
     
 
-}
+// }
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./components/Home";
+ 
 import CreateMenu from "./views/CreateMenu";
 import DietTemplate from "./views/DietTemplate";
+import Resume from "./views/Resume";
+import App from "./App";
 
  const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />,
-        children: [{
+        element: <App />,
+        children: [
+            {
+                path:'/',
+                element: <Resume />
+            },
+            {
             path: '/create-menu',
             element:  <CreateMenu />
-        },{
+            },{
             path: '/diet',
             element:  <DietTemplate />
-        }]
+            }
+        ]
     },
     
 ])
